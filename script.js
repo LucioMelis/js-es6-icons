cf9c1ac76807b5b3e08789878f7242aa1092ad9d
luciomelis <luciomelis1992@gmail.com>
2022-03-23 18:27:59 +0100

    changes

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 console.log('Js Ok');
 
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona,
-// in cui è presente il nome dell'icona e l'icona stessa.
-
-/* <i class="fa-solid fa-cat"></i> */
-
-const Icone = [
+const arrayIcone = [
     {
         name: 'cat',
         prefix: 'fa-',
@@ -120,3 +114,34 @@ const Icone = [
         color: 'blue'
     }
 ];
+
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona,
+// in cui è presente il nome dell'icona e l'icona stessa.
+
+// Milestone 2
+// Ciascuna icona ha una proprietà "color":
+// utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+
+
+const containerGenerale = document.getElementById('container-generale');
+
+
+arrayIcone.forEach((elemento) => {
+    elementoCreato(elemento);
+})
+
+function elementoCreato(arrayIndice) {
+
+    const containerElemento = document.createElement('div');
+    containerElemento.classList.add('col-2', 'text-center', 'border', 'py-2');
+
+    containerElemento.innerHTML = `
+    <i class="${arrayIndice.family} ${arrayIndice.prefix}${arrayIndice.name} ${arrayIndice.color} "></i>
+    <p>${arrayIndice.name}</p>`
+
+    containerGenerale.appendChild(containerElemento);
+}
+
+
+
